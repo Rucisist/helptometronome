6405e7f9b66419fb6d34834578750a102b90a62b
MDDPAEEGproject <andreyilse@yandex.ru>
2017-10-22 10:51:12 +0300

    Initial Commit

---
+++ helptometro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helptometro
+//
+//  Created by Андрей Илалов on 22.10.2017.
+//  Copyright © 2017 Андрей Илалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
